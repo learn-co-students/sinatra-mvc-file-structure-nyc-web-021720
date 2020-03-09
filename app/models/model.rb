e14a0958c496d1d6eefe7904a4447ef608694395
osgoodgunawan <ogunawan@ucsd.edu>
2020-03-09 17:50:43 -0400

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,17 @@
-class Model
+class Dog 
+    attr_accessor :name, :breed, :age
+    @@all=[]
+
+    def initialize (name,breed,age)
+        @name=name
+        @breed=breed
+        @age=age
+        Dog.all << self
+    end
+    
+    def self.all
+        @@all
+    end
+
 
 end
